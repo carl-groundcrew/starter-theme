5f904104d98722c930a789379c3fc8dfe4472175
Carl Beaverson <carlbeaverson@gmail.com>
2021-02-16 09:59:22 +1000

    Change to Starter theme

--- functions.php
+++ functions.php
@@ -6,6 +6,24 @@
  * @since   Timber 0.1
  */
 
+if ( ! class_exists( 'Timber' ) ) {
+
+	add_action(
+		'admin_notices',
+		function() {
+			echo '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="' . esc_url( admin_url( 'plugins.php#timber' ) ) . '">' . esc_url( admin_url( 'plugins.php' ) ) . '</a></p></div>';
+		}
+	);
+
+	add_filter(
+		'template_include',
+		function( $template ) {
+			return get_stylesheet_directory() . '/static/no-timber.html';
+		}
+	);
+	return;
+}
+
 
 /**
  * Sets the directories (inside your theme) to find .twig files
